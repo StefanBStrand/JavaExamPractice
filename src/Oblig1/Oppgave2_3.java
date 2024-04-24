41504ed10964b231780d3e06ea7e536680491c5e
Stefan Strand <stefanboytchev@hotmail.com>
2024-04-24 21:58:38 +0200

    continue on oblig 1 practice

---
+++ src/Oblig1/Oppgave2_3.java
@@ -0,0 +1,8 @@
+package Oblig1;
+
+public class Oppgave2_3 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Oblig1/Planet.java
@@ -0,0 +1,41 @@
+package Oblig1;
+
+public class Planet {
+    private String name;
+
+    private double radius;
+
+    private double mass;
+
+    public Planet(String name, double radius, double mass) {
+        this.name = name;
+        this.radius = radius;
+        this.mass = mass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    //REMEMBER - SETTERS are VOID - they do not RETURN ANYTHING
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+}
