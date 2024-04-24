92302cb767ecf73724f6e23378c9e497cc6669f9
Stefan Strand <stefanboytchev@hotmail.com>
2024-04-24 21:01:03 +0200

    Practice from Oblig1

--- src/Oblig1/Main1.java
+++ src/Oblig1/Main1.java
@@ -1,7 +1,22 @@
 package Oblig1;
 
 public class Main1 {
+
+    //OBLIG 1 Oppgave 2.1
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        //Creating an instance of MoonGravity to be run here in main
+        MoonGravity gravity = new MoonGravity();
+
+        double earthWeight = 94; // My weight on earth
+        // Assigning method call to Variable - STORING the RETURN value for later Use!
+        double moonWeight = gravity.weightOnTheMoon(earthWeight);
+        System.out.println("Weight on the Moon: " + String.format("%.2f", moonWeight)  + "kg.");
+
+        // Direct method call WITHOUT assigning it to variable - data NOT POSSIBLE to USE LATER - Will not be stored.
+        gravity.weightOnTheMoon(earthWeight);
+
     }
 }
---
+++ src/Oblig1/MoonGravity.java
@@ -0,0 +1,8 @@
+package Oblig1;
+
+public class MoonGravity {
+
+    public double weightOnTheMoon(double weightOnEarth) {
+        return weightOnEarth * 0.165;
+    }
+}
