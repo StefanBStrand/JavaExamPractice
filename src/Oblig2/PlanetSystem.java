f7da3619baa1a1171f196fec6f39e8c30d7b5011
Stefan Strand <stefanboytchev@hotmail.com>
2024-05-09 22:15:45 +0200

    practice

--- src/Oblig2/PlanetSystem.java
+++ src/Oblig2/PlanetSystem.java
@@ -1,4 +1,25 @@
 package Oblig2;
 
+import java.util.ArrayList;
+
 public class PlanetSystem {
+
+    private String name;
+
+    private Star centerStar;
+
+    private ArrayList<Planet> planets;
+
+    public String getName() {
+        return name;
+    }
+
+    public Star getCenterStar() {
+        return centerStar;
+    }
+
+    public ArrayList<Planet> getPlanets() {
+        return planets;
+    }
+
 }
